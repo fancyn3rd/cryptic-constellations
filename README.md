1f219bf1116ad33c5dc615fd9c210a0e29ca0665
fancyn3rd <>
2020-12-21 18:48:49 +0100

    Basic constellation

--- README.md
+++ README.md
@@ -1,31 +1,6 @@
-![](cover.png)
 
-# Fancy Pattern Generator
+# Constellation Generator
 
-A webapp that creates random texture patterns, which you can save as images.
-It is based on [pixi.js](https://www.pixijs.com/).
+ TODO
 
-**How it works:**
-A few different shapes get repeated and overlayed with different blend modes.
-All options get choosen randomly.
-
-## Usage
-
-Open index.html in the dist folder.
-
-Hotkeys:
-* G - Generate new pattern
-* S - Save pattern to disk
-
-## Development
-
-* `npm install`
-* `npm run start`
-
-You can change the pattern generation settings in config.js.
-
-### TODO
-
-* User Interface
-* More options: Pattern size etc.
-* New Modes
+ [pixi.js](https://www.pixijs.com/).
--- src/canvas.js
+++ src/canvas.js
@@ -1,83 +1,133 @@
 
 import * as PIXI from "pixi.js";
 
-import { 
-  colors,
-  shapeTypes,
-  blendModes,
-  maxLineStrength,
-  maxLoop
-} from "./config.js"
-
-export const canvas = document.getElementById("canvas")
-
-canvas.getContext("webgl",  {
-  preserveDrawingBuffer: true
-})
-
 const container = new PIXI.Container();
-const RNDShape = new PIXI.Graphics();
 
 const app = new PIXI.Application(
-  canvas.width,
-  canvas.height, {
+  800,
+  600, {
     antialias: true,
-    view: canvas,
-    backgroundColor : colors[randomRange(0, colors.length)]
+    backgroundColor : "black"
   }
 );
 
+
 app.stage.addChild(container);
 document.body.appendChild(app.view);
-container.addChild(RNDShape);
+
+let mouseTrack = 0
+
+let stars = []
+let isDone = false
+
+const MAX_STARS = 12
+const MIN_STARS = 5
+const MIN_MOUSETRACK = 10
+const MAX_MOUSETRACK = 40
+
+let mousetrackToNextStar
+let constellationStarCount
+
+document.body.addEventListener("mousemove", (event) => onMouseMove(event))
+document.body.addEventListener("click", () => resetCanvas())
+setMouseTrackToNextStar()
+setConstellationStarCount()
+
+function setConstellationStarCount() {
+  constellationStarCount = randomRange(MIN_STARS, MAX_STARS)
+}
+
+function resetCanvas() {
+  stars = []
+  container.removeChildren()
+  isDone = false
+  mouseTrack = 0
+}
+
+
+function setMouseTrackToNextStar() {
+  mousetrackToNextStar = randomRange(MIN_MOUSETRACK, MAX_MOUSETRACK)
+}
+
+
+function onMouseMove(mouseEvent) {
+  mouseTrack += 1
+  if (mouseTrack > mousetrackToNextStar) {
+
+    if (stars.length === constellationStarCount) {
+      connectStars()
+      stars = []
+      isDone = true
+    }
+
+    if (!isDone) {
+      createStarPosition(mouseEvent)
+      setMouseTrackToNextStar()
+      mouseTrack = 0
+
+    }
+
+  }
+}
+
+
+
 
 //*******************************************************************************************
 
-export function createPattern() { 
-  app.renderer.backgroundColor = colors[randomRange(0, colors.length)]
-  RNDShape.clear()
-    
-  for (let i=0; i<3; i++) {
-    initShape(RNDShape)
-    drawLoop(RNDShape)
+
+
+function createStarPosition(mouseEvent) {
+  console.log("draw star")
+  drawStar(mouseEvent)
+}
+
+const styles = {
+  star: {
+    line: {
+      weight: 1,
+      color:  PIXI.utils.string2hex("#ffffff")
+    },
+    color:  PIXI.utils.string2hex("#ffffff"),
+    size: 5
+  },
+  line: {
+    weight: 2,
+    color:  PIXI.utils.string2hex("#ffffff")
   }
 }
 
-function initShape(shape) {
-  shape.posX = randomRange(5, 10)
-  shape.posY = randomRange(5, 10)
-  shape.sizeW = randomRange(10, 20)
-  shape.sizeH = randomRange(10, 20)
-  shape.distanceX = shape.sizeW*3
-  shape.distanceY = shape.sizeH*2
-  shape.type = shapeTypes[randomRange(0, shapeTypes.length)]
-  shape.lineStrength = randomRange(0,maxLineStrength)
-  shape.fillColor = colors[randomRange(0, colors.length)]
-  shape.blendMode = PIXI.BLEND_MODES[blendModes[randomRange(1, blendModes.length)]]
+function drawStar(mouseEvent) {
+  const star = new PIXI.Graphics();
+  star.lineStyle(styles.star.line.weight, styles.star.line.color, 1);
+  star.beginFill(styles.star.color, 1);
+  star.drawCircle(0, 0, randomRange(2, 5))
+  star.position.set(mouseEvent.x, mouseEvent.y)
+  star.endFill();
+  container.addChild(star);
+  stars.push(star)
 }
 
-
-function drawLoop(shape) {
-  for (let y = 0; y <= maxLoop; y ++) {
-    shape.beginFill(shape.fillColor, 1)
-    for (let x = 0; x <= maxLoop; x++) {
-        shape.lineStyle(shape.lineStrength, colors[randomRange(0, colors.length)]);
-        draw(shape, y, x)
-    }
+async function connectStars() {
+  for (var i = 0; i < MAX_STARS; i++) {
+    drawLine(stars[i], stars[i + 1])
   }
-  shape.endFill()
+  drawLine(stars[randomRange(0, stars.length - 1)], stars[randomRange(0, stars.length - 1)])
+  drawLine(stars[randomRange(0, stars.length - 1)], stars[randomRange(0, stars.length - 1)])
 }
 
-function draw(shape, countY, countX) {
-    shape["draw" + shape.type](
-        shape.posX + countX * shape.distanceX,
-        shape.posY + countY * shape.distanceY, 
-        shape.sizeW,
-        shape.sizeH,
-        shape.sizeW
-    );
+function drawLine(pointA, pointB) {
+  if (pointA && pointB) {
+    const line = new PIXI.Graphics()
+    line.lineStyle(styles.line.weight, styles.line.color, 1);
+    line.moveTo(pointA.x, pointA.y);
+    line.lineTo(pointB.x, pointB.y)
+    container.addChild(line);
+  }
 }
 
 function randomRange(min, max) {
-  return Math.floor(Math.random() * (max - 1 - min + 1) + min)
+  return Math.ceil(Math.random() * (max - min) + min)
 }
+
+
