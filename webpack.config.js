1fee36f6b65758c04eba470ae7be9d20e12e9d92
fancyn3rd <>
2020-12-24 17:16:56 +0100

    Add stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,41 @@
+
+const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path")
 
 module.exports = {
+  devtool: "eval-source-map",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "./bundle.js"
+},
   module: {
     rules: [
-      {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
+        {
+          test: /\.css$/i,
+          use: ['css-loader']
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
           }
-        ]
-      }
-    ]
+        }
+      ],
   },
+  devServer: {
+    publicPath:"/",
+    contentBase: path.resolve(__dirname, "./")
+  },
+  node: {
+    fs: "empty"
+ },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
+      template: "./index.html",
       filename: "./index.html"
     })
-    // new CopyWebpackPlugin([{
-    //   from: "./src/assets", to: "./assets"
-    // }])
   ]
-};
+}
+
