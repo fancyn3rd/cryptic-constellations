a9d3ca8ade740cc2c0e15186fd6fe43cf63deccb
fancyn3rd <>
2020-12-27 12:39:19 +0100

    Cleanup

--- src/canvas.js
+++ src/canvas.js
@@ -31,6 +31,9 @@ let mousetrackToNextStar
 let constellationStarCount
 let oldMousePos = []
 
+document.body.addEventListener("pointermove", (event) => onPointerMove(event))
+document.body.addEventListener("pointerdown", (event) => onPointerClick(event))
+
 const starContainer = new PIXI.Container()
 setupContainer(starContainer)
 
@@ -40,25 +43,11 @@ setupContainer(lineContainer)
 const backgroundContainer = new PIXI.Container()
 setupContainer(backgroundContainer)
 
-document.body.addEventListener("pointermove", (event) => onMouseMove(event))
-document.body.addEventListener("pointerdown", (event) => onClick(event))
-
-document.body.appendChild(app.view);
-
-app.stage.addChild(backgroundContainer)
-app.stage.addChild(lineContainer);
-app.stage.addChild(starContainer);
-
-drawBackgroundStars()
-setMouseTrackToNextStar()
-setConstellationStarCount()
-
 const introText = new PIXI.Text(`Use ${USER_INPUT_DEVICE} to f1nd a const3llation`, introTextStyle);
 introText.pivot.x = introText.width / 2
 introText.pivot.y = introText.height / 2
 introText.x = app.screen.width/2
 introText.y = app.screen.height/2 - 20
-app.stage.addChild(introText);
 
 const constellationNameText = new PIXI.Text(
   firstLetterUpperCase(nouns[randomRange(0, nouns.length - 1)]) + " "
@@ -66,7 +55,17 @@ const constellationNameText = new PIXI.Text(
   + "X", nameTextStyle
 )
 constellationNameText.visible = false
+
+document.body.appendChild(app.view);
+app.stage.addChild(backgroundContainer)
+app.stage.addChild(lineContainer);
+app.stage.addChild(starContainer);
+app.stage.addChild(introText);
 app.stage.addChild(constellationNameText);
+
+drawBackgroundStars()
+setMouseTrackToNextStar()
+setConstellationStarCount()
 
 app.ticker.add((delta) => {
   if (isConstellationRoating) {
@@ -88,7 +87,7 @@ function setupContainer(container) {
   container.pivot.y = app.screen.height / 2
 }
 
-function onClick(event) {
+function onPointerClick(event) {
   if (isConstellationRoating) {
     resetCanvas(event)
   }
@@ -113,70 +112,70 @@ function resetCanvas(event) {
     + arabToRoman(randomRange(0,20)), nameTextStyle
 }
 
-function onMouseMove(mouseEvent) {
+function onPointerMove(event) {
   mouseTrack += 1
   if (mouseTrack > mousetrackToNextStar) {
 
-    if (isMousePosDifferent("x", mouseEvent, MIN_AXIS_DIFFERENCE) &&
-      isMousePosDifferent("y", mouseEvent, MIN_AXIS_DIFFERENCE)) {
-      oldMousePos.x = mouseEvent.x
-      oldMousePos.y = mouseEvent.y
+    if (isMousePosDifferent("x", event, MIN_AXIS_DIFFERENCE) &&
+      isMousePosDifferent("y", event, MIN_AXIS_DIFFERENCE)) {
+        oldMousePos.x = event.x
+        oldMousePos.y = event.y
 
-        if (stars.length === constellationStarCount) {
-        connectStars()
+          if (stars.length === constellationStarCount) {
+            connectStars()
 
-        const rndColor = colors[randomRange(0, colors.length - 1)]
-        tintChilds(starContainer, rndColor)
-        tintChilds(lineContainer, rndColor)
+            const rndColor = colors[randomRange(0, colors.length - 1)]
+            tintChilds(starContainer, rndColor)
+            tintChilds(lineContainer, rndColor)
 
-        stars = []
-        isConstellationDrawn = true
-        constellationNameText.visible = true
-        constellationNameText.pivot.x = constellationNameText.width / 2
-        constellationNameText.pivot.y = constellationNameText.height / 2
-        constellationNameText.x = app.screen.width/2
-        constellationNameText.y = app.screen.height - 50
-        setTimeout(() => isConstellationRoating = true, 1000)
+            stars = []
+            isConstellationDrawn = true
+            constellationNameText.visible = true
+            constellationNameText.pivot.x = constellationNameText.width / 2
+            constellationNameText.pivot.y = constellationNameText.height / 2
+            constellationNameText.x = app.screen.width/2
+            constellationNameText.y = app.screen.height - 50
+            setTimeout(() => isConstellationRoating = true, 1000)
+          }
+
+        if (!isConstellationDrawn) {
+          createStarPosition(event)
+          setMouseTrackToNextStar()
+          mouseTrack = 0
+        }
       }
-
-      if (!isConstellationDrawn) {
-        createStarPosition(mouseEvent)
-        setMouseTrackToNextStar()
-        mouseTrack = 0
-      }
-    }
   }
 }
 
-function createStarPosition(mouseEvent) {
+function createStarPosition(event) {
   const randomValue = randomRange(0,100)
   switch(true) {
     case (randomValue <= 50):
-      drawCircle(mouseEvent)
+      drawCircle(event)
       break
     case (randomValue > 50):
-      drawStar(mouseEvent)
+      drawStar(event)
       break
   }
 }
 
-function drawCircle(mouseEvent) {
+function drawCircle(event) {
   const star = new PIXI.Graphics();
   star.lineStyle(styles.star.line.weight, styles.star.line.color, 1);
   star.beginFill(styles.star.color, 1);
   star.drawCircle(0, 0, randomRange(2, 5), randomRange(2, 5))
-  star.position.set(mouseEvent.x, mouseEvent.y)
+  star.position.set(event.x, event.y)
   star.endFill();
   starContainer.addChild(star);
   stars.push(star)
 }
 
-function drawStar(mouseEvent) {
+function drawStar(event) {
   const star = new PIXI.Graphics();
   star.lineStyle(styles.star.line.weight, styles.star.line.color, 1);
   star.beginFill(styles.star.color, 1);
   star.drawStar(0, 0, randomRange(3, 10), randomRange(2, 8))
-  star.position.set(mouseEvent.x, mouseEvent.y)
+  star.position.set(event.x, event.y)
   star.endFill();
   starContainer.addChild(star);
   stars.push(star)
