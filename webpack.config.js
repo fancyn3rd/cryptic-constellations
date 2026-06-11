ea642f557cbd0fed25060e9786a1b63e3a4057be
Till G <>
2026-06-11 11:23:20 +0200

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,55 @@
-
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const webpack = require('webpack')
-const path = require("path")
+const webpack = require("webpack");
+const path = require("path");
 
-module.exports = (env) => ({
-  devtool: env.development ? "eval-source-map" : "none",
-  mode: env.development ? "development" : "prouduction",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "./bundle.js"
-},
-  module: {
-    rules: [
+module.exports = (env) => {
+  const isEnvDevelopment = !!env.development;
+
+  return {
+    devtool: isEnvDevelopment ? "eval-source-map" : false,
+
+    mode: isEnvDevelopment ? "development" : "production",
+
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+      clean: true,
+    },
+    module: {
+      rules: [
         {
           test: /\.css$/i,
-          use: ['css-loader']
+          use: ["style-loader", "css-loader"],
         },
         {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader"
-          }
-        }
+            loader: "babel-loader",
+          },
+        },
       ],
-  },
-  devServer: {
-    static: path.resolve(__dirname, "./")
-  },
-  resolve: {
-    fallback: { 
-      "url": require.resolve("url/")
-    }
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./index.html",
-      filename: "./index.html"
-    }),
-    new webpack.ProvidePlugin({
-      process: 'process/browser'
-    })
-  ]
-})
-
+    },
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, "./"),
+      },
+      // Enables hot module replacement
+      hot: true,
+    },
+    resolve: {
+      fallback: {
+        url: require.resolve("url"),
+      },
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./index.html",
+        filename: "index.html",
+      }),
+      new webpack.ProvidePlugin({
+        process: "process/browser",
+      }),
+    ],
+  };
+};
